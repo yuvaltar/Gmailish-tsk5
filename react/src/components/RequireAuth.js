5e445e45a3db293de0dc72a7197dac696d19d7cd
itaysmouha <itaysmouha@gmail.com>
2025-06-26 13:05:15 +0300

    make gradle files work

--- react/src/components/RequireAuth.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
-
-const RequireAuth = ({ children }) => {
-  const token = localStorage.getItem("token");
-  return token ? children : <Navigate to="/login" replace />;
-};
-
-export default RequireAuth;
